75bd34680978fafc77fb36957ef186a11ad9d01d
仁一 伊藤 <150215@jc-21.jp>
2018-01-25 09:38:46 +0900

    day+5

--- MuscleTrainingRecords00/MuscleTrainingRecords00/GraphPage.xaml.cs
+++ MuscleTrainingRecords00/MuscleTrainingRecords00/GraphPage.xaml.cs
@@ -49,7 +49,7 @@ namespace MuscleTrainingRecords00
                 //Boolean bDone = eDone.IsToggled;
                 int B_Weight = int.Parse(bWeight.Text);
                 int B_Fat = int.Parse(bFat.Text);
-                DateTime dCreated = DateTime.Today.AddDays(+4);
+                DateTime dCreated = DateTime.Today.AddDays(+5);
 
 
                 TodoItem sameDateItem = await db.GetItemByCreatedAsync(dCreated);
